d679efd39084b0127101c92141cd63825fe9ed20
ozyllha <ozyllha@pm.me>
2026-06-25 21:32:21 -0300

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'litorale-v1';
+const FILES = ['/', '/index.html', '/admin.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
